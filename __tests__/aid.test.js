313b6c5fd84a3f671defd1679bba70090448fd66
Neisser <neisser@soluntech.com>
2023-06-06 21:20:32 -0500

    doc(package.json, calcifer.test.js): test modified

--- __tests__/aid.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const aid = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(aid(), 'Hello from aid');
-console.info('aid tests passed');
---
+++ __tests__/calcifer.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const calcifer = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(calcifer(), 'Hello from calcifer');
+console.info('calcifer tests passed');
